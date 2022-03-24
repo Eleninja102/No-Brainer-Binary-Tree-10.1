98a52886a96041d4497f2414d55bdbf9dd590bce
Coleton Watt <electricninja102@gmail.com>
2022-03-23 18:47:35 -0600

    Finished No Brainer

--- No Brainer: Binary Tree 10.1/main.cpp
+++ No Brainer: Binary Tree 10.1/main.cpp
@@ -4,11 +4,3 @@
 //
 //  Created by Coleton Watt on 3/21/22.
 //
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
